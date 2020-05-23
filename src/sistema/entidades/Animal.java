c9b292dbb36ecd16512a450d44a3952dc2864606
matheus <matheuslima20111997@gmail.com>
2020-05-23 16:39:18 -0300

    First Commit

---
+++ src/sistema/entidades/Animal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.entidades;
+
+/**
+ *
+ * @author MATHEUS-PC
+ */
+public class Animal {
+    private int idAnimal;
+    private String NmAnimal;
+    private String cor;
+    private String raca;
+    private String especie;
+   
+
+}
---
+++ src/sistema/principal/Conexao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.principal;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Conexao {
+
+    public static String servidor = "jdbc:sqlserver://localhost:1433; databaseName=sistema_funcionarios";
+    public static String usuario = "sa";
+    public static String senha = "sanmat9242";
+}
